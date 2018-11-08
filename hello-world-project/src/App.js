8753a4c4da6642667b1b1d2527889701251a8bd5
Sue <suekozuu@gmail.com>
2018-11-08 19:25:48 +0700

    Create project and Navbar

---
+++ hello-world-project/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Navbar from './Component/Landing/Navbar';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="col-11 offset-1">
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        <h1>000000000000</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hello-world-project/src/Component/Landing/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react' ;
+import styled from 'styled-components' ;
+
+const Navdiv = styled.div`
+    position: fixed;
+`
+
+const Icon = styled.img`
+    margin: 15px;
+    width: 70px;
+    &:hover{
+        transform: rotate(360deg);
+        transition: transform .8s ease-in-out;
+        opacity: 0.8;
+    }
+
+`
+
+const Navbar =()=>(
+    <Navdiv className="col-1">
+    <a><Icon src='./images/1What.svg' /></a>
+    <a><Icon src='./images/2Who.svg' /></a>
+    <a><Icon src='./images/3Where.svg' /></a>
+    <a><Icon src='./images/4When.svg' /></a>
+    <a><Icon src='./images/5FAQs.svg' /></a>
+    <a><Icon src='./images/6Contact.svg' /></a>
+
+    </Navdiv>
+)
+
+export default Navbar
+

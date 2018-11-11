da3e7a65cdec805363146cb4c9af05177bde8374
zeeza <zeezaunicorn@outlook.com>
2018-11-12 00:24:38 +0700

    When and Where

--- src/Component/When/When.js
+++ src/Component/When/When.js
@@ -7,13 +7,77 @@ const WhenDiv = styled.div`
     height: 100%;
     top: 400%;
 `
+const TitleText = styled.div`
+    text-align: center;
+`
+
+const ImgTimeline = styled.img`
+    margin-left: 330px;
+
+`
+const Img = styled.img`
+    margin-left: 63px;
+
+`
+const Img2 = styled.img`
+    margin-left: 30px;
+
+`
+const Img3 = styled.img`
+    margin-left: 99px;
+
+`
+const Img4 = styled.img`
+    margin-left: 65px;
+
+`
+const Img5 = styled.img`
+    margin-left: 18px;
+
+`
+const Img6 = styled.img`
+    margin-left: 130px;
+
+`
 
 const When =()=>(
     <div>
         <a name="When">
         <WhenDiv className="col-sm-11 offset-sm-1">
-            Whennnn
+          <ImgTimeline src='./images/when/Timeline.svg' whidth = '200' height = '55'/>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+          <br></br>
+
+          <Img src='./images/when/T1.1.svg' whidth = '200' height = '70'/>
+          <Img src='./images/when/line1.svg' whidth = '10' height = '8'/>
+          <Img src='./images/when/T2.1.svg' whidth = '200' height = '70'/>
+          <Img src='./images/when/line1.svg' whidth = '10' height = '8'/>
+          <Img src='./images/when/T3.1.svg' whidth = '200' height = '70'/>
+          <Img src='./images/when/line1.svg' whidth = '10' height = '8'/>
+          <Img src='./images/when/T4.1.svg' whidth = '200' height = '70'/>
+          <br></br>
+
+          <Img2 src='./images/when/T1.2.svg' whidth = '10' height = '20'/>
+          <Img3 src='./images/when/T2.2.svg' whidth = '10' height = '20'className="col-3"/>
+          <Img2 src='./images/when/T3.2.svg' whidth = '10' height = '20'className="col-3"/>
+          <Img4 src='./images/when/T4.2.svg' whidth = '10' height = '20'className="col-2"/>
+          <br></br>
+
+          <Img5 src='./images/when/T1.3.svg' whidth = '200' height = '15'/>
+          <Img6 src='./images/when/T2.3.svg' whidth = '200' height = '20'className="col-2"/>
+          <Img6 src='./images/when/T3.3.svg' whidth = '200' height = '20'className="col-2"/>
+          <Img3 src='./images/when/T4.3.svg' whidth = '200' height = '35'className="col-2"/>
+
+
+
+
+
         </WhenDiv>
+
         </a>
     </div>
 
--- src/Component/Where/Where.js
+++ src/Component/Where/Where.js
@@ -8,12 +8,42 @@ const WhereDiv = styled.div`
     top: 300%;
 `
 
+const TitleText = styled.div`
+    text-align: center;
+    color:#a6a6a6
+`
+
+const Img = styled.img`
+    margin-left: 300px;
+
+`
+const Img2 = styled.img`
+    margin-bottom: 100px;
+    margin-left: 200px;
+
+`
+
 const Where =()=>(
     <div>
+
         <a name="Where">
+
         <WhereDiv className="col-sm-11 offset-sm-1">
-            Whereeeee
+          <Img src='./images/where/Where.svg' whidth = '150' height = '60'/>
+        <br></br>
+        <br></br>
+        <Img src='./images/where/3.svg' whidth = '1000' height = '400'/>
+        <br></br>
+        <br></br>
+        <TitleText><h6>*** อาคารเรียนรวม 2 ***</h6></TitleText>
+
+
+
+
+
+
         </WhereDiv>
+
         </a>
     </div>
 
